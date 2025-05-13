7479b77ed6e1cc7e7ff10812605b47cd10cba6c3
Ekansh Gupta <ekansh.gupta@oss.qualcomm.com>
2025-05-13 09:58:21 +0530

    FROMLIST: misc: fastrpc: Add NULL check to fastrpc_buf_free to prevent crash
    The fastrpc_buf_free function currently does not handle the case where
    the input buffer pointer (buf) is NULL. This can lead to a null pointer
    dereference, causing a crash or undefined behavior when the function
    attempts to access members of the buf structure. Add a NULL check to
    ensure safe handling of NULL pointers and prevent potential crashes.
    
    Link: https://lore.kernel.org/all/20250513042825.2147985-2-ekansh.gupta@oss.qualcomm.com/
    Fixes: c68cfb718c8f9 ("misc: fastrpc: Add support for context Invoke method")
    Cc: stable@kernel.org
    Signed-off-by: Ekansh Gupta <ekansh.gupta@oss.qualcomm.com>

--- drivers/misc/fastrpc.c
+++ drivers/misc/fastrpc.c
@@ -394,6 +394,9 @@ static int fastrpc_map_lookup(struct fastrpc_user *fl, int fd,
 
 static void fastrpc_buf_free(struct fastrpc_buf *buf)
 {
+	if (!buf)
+		return;
+
 	dma_free_coherent(buf->dev, buf->size, buf->virt,
 			  FASTRPC_PHYS(buf->phys));
 	kfree(buf);
